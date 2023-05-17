bc87c2562f330fc7fab7b228346b8e96b22efda5
Afzal Diwan <afzalhusen3@gmail.com>
2023-05-17 10:39:46 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import NavBar from "./components/NavBar";
+
+const App = () => {
+  return (
+    <Fragment>
+      <NavBar />
+    </Fragment>
+  );
+};
+
+export default App;
